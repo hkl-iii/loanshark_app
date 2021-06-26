3dafb1c3ac6aa91eb9c54055ee5083269e233141
hkl-iii <rahleggon@gmail.com>
2021-06-26 07:14:17 +0200

    loans model updated

--- backend/loanshark/money_loans/models.py
+++ backend/loanshark/money_loans/models.py
@@ -6,6 +6,7 @@ from accounts.models import User
 class Loans(models.Model):
     user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
     amount = models.CharField(max_length=30, blank=True, null=True)
+    total_amount_to_pay = models.CharField(max_length=30, blank=True, null=True)
     is_verified = models.BooleanField(default=False)
     is_done = models.BooleanField(default=False)
     
@@ -13,6 +14,11 @@ class Loans(models.Model):
     
     def __str__(self):
         return str(self.user.email)
+
+    def calculate_total_amount(self):
+        amount = self.amount
+        total = (amount * 30) / 100
+        return total
 
     class Meta:
         verbose_name_plural = 'Loans'
